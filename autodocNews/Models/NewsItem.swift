a265face159de9975257ec7f018e26b3aac88144
abayellow <sasha.abanshin@gmail.com>
2025-10-29 18:11:14 +1100

    Update model

---
+++ autodocNews/Models/NewsItem.swift
@@ -0,0 +1,19 @@
+//
+//  NewsItem.swift
+//  autodocNews
+//
+//  Created by Alexander Abanshin on 27.10.2025.
+//
+
+import Foundation
+
+struct APIModel: Codable  {
+    let news: [News]
+}
+
+nonisolated
+struct News: Codable, Hashable {
+    let title: String
+    let titleImageUrl: URL
+    let fullUrl: URL
+}
