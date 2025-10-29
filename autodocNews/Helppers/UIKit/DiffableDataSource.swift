dc4871b5a5db57df38f3bd108efb2f52ddd82f20
abayellow <sasha.abanshin@gmail.com>
2025-10-29 18:10:50 +1100

    Add shimmer + delete unused files

--- autodocNews/Helppers/UIKit/DiffableDataSource.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DiffableDataSource.swift
-//  autodocNews
-//
-//  Created by Alexander Abanshin on 27.10.2025.
-//
-
-import Foundation
--- autodocNews/Modules/NewsModule/NewsViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NewsViewModel.swift
-//  autodocNews
-//
-//  Created by Alexander Abanshin on 27.10.2025.
-//
-
-import Foundation
